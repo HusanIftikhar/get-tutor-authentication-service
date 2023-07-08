fd08337cf6a604858b66c349f7d13217ccf9aeb1
HusanIftikhar <engr.iftikhar100@hotmail.com>
2023-07-08 23:47:25 +0200

    init commit

---
+++ src/main/java/servic/auth/get/tutors/gettutors/GetTutorsApplication.java
@@ -0,0 +1,13 @@
+package servic.auth.get.tutors.gettutors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetTutorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetTutorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/servic/auth/get/tutors/gettutors/GetTutorsApplicationTests.java
@@ -0,0 +1,13 @@
+package servic.auth.get.tutors.gettutors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetTutorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
